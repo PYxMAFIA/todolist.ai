54e120736890c1a15a94e9a4fd80968a4beabdd4
Piyus Kumar <prince18401015@gmail.com>
2025-06-16 14:56:37 +0530

    going live

--- app.js
+++ app.js
@@ -75,6 +75,8 @@ passport.deserializeUser(function(id, done) {
   });
 });
 
+console.log("Using redirect URI:", process.env.GOOGLE_CALLBACK_URL);
+
 passport.use(new GoogleStrategy({
     clientID: process.env.GOOGLE_CLIENT_ID,
     clientSecret: process.env.GOOGLE_CLIENT_SECRET,
