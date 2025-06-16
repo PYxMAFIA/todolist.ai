801c7a35424bddf2c6a461dcf812daf74c4c8ad9
Piyus Kumar <prince18401015@gmail.com>
2025-06-16 14:34:31 +0530

    using env

--- app.js
+++ app.js
@@ -78,7 +78,7 @@ passport.deserializeUser(function(id, done) {
 passport.use(new GoogleStrategy({
     clientID: process.env.GOOGLE_CLIENT_ID,
     clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    callbackURL: "https://todolist-ai.onrender.com/auth/google/callback",
+    callbackURL: "process.env.GOOGLE_CALLBACK_URL",
     userProfileURL: "https://www.googleapis.com/oauth2/v3/userinfo"
   },
   async function(accessToken, refreshToken, profile, cb) {
